fab4aa26b01af68b66fc0cd2f0c9711dcbbc575e
Marko Vujnovic <mv@becklyn.com>
2022-07-15 14:46:44 +0200

    Added support for Oracle

--- src/Infrastructure/DoctrineMigrations/Version20220408113020.php
+++ src/Infrastructure/DoctrineMigrations/Version20220408113020.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Becklyn\Ddd\PersonalData\Infrastructure\DoctrineMigrations;
 
+use Doctrine\DBAL\Platforms\OraclePlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -21,7 +22,11 @@ final class Version20220408113020 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql('CREATE TABLE personal_data_store (id VARCHAR(36) NOT NULL, aggregate_id VARCHAR(36) NOT NULL, personal_value VARCHAR(255) DEFAULT NULL, days_to_live INT NOT NULL, event_raised_ts DATETIME(6) NOT NULL, expires_ts DATETIME(6) NOT NULL, anonymized_ts DATETIME(6) DEFAULT NULL, created_ts DATETIME(6) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        if ($this->platform instanceof OraclePlatform) {
+            $this->addSql('CREATE TABLE personal_data_store (id VARCHAR2(36) NOT NULL, aggregate_id VARCHAR2(36) NOT NULL, personal_value VARCHAR2(255) DEFAULT NULL NULL, days_to_live NUMBER(10) NOT NULL, event_raised_ts TIMESTAMP(6) NOT NULL, expires_ts TIMESTAMP(6) NOT NULL, anonymized_ts TIMESTAMP(6) DEFAULT NULL NULL, created_ts TIMESTAMP(6) NOT NULL, PRIMARY KEY(id))');
+        } else {
+            $this->addSql('CREATE TABLE personal_data_store (id VARCHAR(36) NOT NULL, aggregate_id VARCHAR(36) NOT NULL, personal_value VARCHAR(255) DEFAULT NULL, days_to_live INT NOT NULL, event_raised_ts DATETIME(6) NOT NULL, expires_ts DATETIME(6) NOT NULL, anonymized_ts DATETIME(6) DEFAULT NULL, created_ts DATETIME(6) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        }
     }
 
     public function down(Schema $schema) : void
